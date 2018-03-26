5f4ba6cfbb656822b182f3967d2023798033dba3
LwcReber <1079670310@qq.com>
2018-03-26 23:10:23 +0800

    sequelize practice

--- server/testserver.js
+++ server/testserver.js
@@ -21,28 +21,86 @@ sequelize
   });
 
 
-  const User = sequelize.define('userlist', {// 定义表
-    name: {
-      type: Sequelize.STRING
-    },
-    password: {
-      type: Sequelize.STRING
+const User = sequelize.define('userlist', { // 定义表
+  id: {
+    type: Sequelize.STRING(50),
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  surePsw: {
+    type: Sequelize.STRING
+  }
+  }, {
+    freezeTableName: true, // 不自动为查找的数据库名称添加 's'
+    timestamps: false
+});
+
+let now = new Date();
+// 增操作
+// User.create({
+//     name: 'Gaffey',
+//     password: 123456,
+//     surePsw: 123456,
+//     createdAt: now,
+// }).then(function (p) {
+//     console.log('created.' + JSON.stringify(p));
+// }).catch(function (err) {
+//     console.log('failed: ' + err);
+// });
+
+// await 写法
+// (async () => {
+//   let newData = await User.create({
+//     name: 'Gaffey',
+//     password: 123456,
+//     surePsw: 123456,
+//     createdAt: now,
+//   });
+//   console.log('created', JSON.stringify(newData));
+// })();
+
+//查操作
+// ( async ()=> {
+//
+//   let getData = await User.findAll({
+//     where: {
+//       name: 'Gaffey'
+//     }
+//   });
+//   console.log(`find ${getData.length} `);
+//   for (let p of getData) {
+//     console.log(JSON.stringify(p));
+//   }
+// }
+// )();
+
+// 改操作
+let param = {'name':'ccd'};
+User.update(param,
+    {
+        'where':{'id': 41}
     }
-  });
+);
 
-  // // force: true 如果表已经存在，将会丢弃表
-  // User.sync({force: true}).then(() => {
-  //   // 表已创建
-  //   return User.create({
-  //     firstName: 'John',
-  //     lastName: 'Hancock'
-  //   });
-  // });
-
-// User.findAll().then(users => {
-//   console.log(users)
+// 删操作
+User.destroy({'where':{'id': 37}});//将表内userId等于23的元组删除
+// // force: true 如果表已经存在，将会丢弃表
+// User.sync({force: true}).then(() => {
+//   // 表已创建
+//   return User.create({
+//     firstName: 'John',
+//     lastName: 'Hancock'
+//   });
+// });
+// User.findAll().then(data => {
+//   console.log(data.get('name'));
 // })
 
-User.findOne().then(user => {
-  console.log('getfName' + user.get('name'));
-});
+// User.findOne().then(user => {
+//   console.log('getfName' + user.get('name'));
+// });
