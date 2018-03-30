0d490755655d72d0eb897decec7711cf6d7a64f9
LwcReber <1079670310@qq.com>
2018-03-30 23:29:00 +0800

    添加sequelize

---
+++ server/server/sqlconfig.js
@@ -0,0 +1,8 @@
+let sqlcon = {
+    host: 'localhost', // 主机名
+    user: 'root', // mysql的用户名
+    password: '123456', // mysql密码
+    port: '3306', // 数据库端口
+    database: 'user' // 操作数据库哪个的表
+};
+module.exports = sqlcon;
